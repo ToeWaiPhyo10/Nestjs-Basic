aff77fb029524f5b91d1581475b3b803bbff3124
viabells-toewai <toewaiphyo@i.viabells.com>
2024-10-20 10:33:19 +0700

    user route

---
+++ src/users/dto/CreateUser.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmpty, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsString()
+  @IsOptional()
+  displayName?: string;
+}
